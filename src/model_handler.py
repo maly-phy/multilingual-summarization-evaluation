cf136cadfb9d6b68c1eb7c1c20731bd9ffe3424a
Mohamed Osama Moussa Mohamed Aly <moh89540@limestone.hpc.ieg.fraunhofer.de>
2025-11-30 17:01:54 +0100

    run with scheduling for api models

---
+++ src/model_handler.py
@@ -0,0 +1,52 @@
+import time, random
+
+
+class ModelHandler:
+    def __init__(self, client, model_name, max_tokens):
+        self.client = client
+        self.model_name = model_name
+        self.max_tokens = max_tokens
+
+    def call_model(self, system_prompt, user_prompt, max_attempts=6, base_delay=3.0):
+        for attempt in range(max_attempts):
+            try:
+                messages = [
+                    {"role": "system", "content": system_prompt},
+                    {"role": "user", "content": user_prompt},
+                ]
+                response = self.base_call_model(messages=messages)
+                response.choices[0].message.content.strip()
+                return response
+
+            except Exception as e:
+                print(f"Attempt {attempt + 1} failed with error: {str(e)}\n")
+
+                if "429" in str(e):
+                    sleep_time = base_delay * (2**attempt + 1) + (
+                        random.randint(0, 1000) / 1000
+                    )
+                    print(
+                        f"Rate limit exceeded. Retrying in {sleep_time/60:.2f} minutes..."
+                    )
+                    time.sleep(sleep_time)
+                    attempt += 1
+                else:
+                    print(f"Error calling model: {str(e)}", flush=True)
+                    break
+
+            finally:
+                time.sleep(base_delay)
+
+    def base_call_model(self, messages):
+        response = self.client.chat.completions.create(
+            messages=messages,
+            model=self.model_name,
+            max_tokens=self.max_tokens,
+            temperature=0.0,
+            top_p=0.95,
+            frequency_penalty=0.5,
+            presence_penalty=0.5,
+            stop=None,
+            n=1,
+        )
+        return response
